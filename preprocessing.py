0a1919904d325dc61c32cb4a3326f8b0a20130ed
Farinam HemmatiZadeh <45711614+farinamhz@users.noreply.github.com>
2021-06-05 23:18:52 +0430

    comment added

--- preprocessing.py
+++ preprocessing.py
@@ -150,7 +150,9 @@ dataset_class, deal_class, progress_dataset = correlation_matrix()
 
 lb = MultiLabelBinarizer()
 lb.fit_transform(dataset_class.values.tolist())
+
 # train
+# default test_size = 0.25
 X_train, X_test, y_train, y_test = train_test_split(dataset_class, deal_class, random_state=0)
 
 
